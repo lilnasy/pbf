cc6415ac027e4ae7eb152071fc35c43bbf6f33ea
lilnasy <emmagoldmanvevo@pm.me>
2022-01-01 10:41:23 +0000

    chore: formatting

--- pbf.ts
+++ pbf.ts
@@ -1,8 +1,9 @@
-import  { readFloatLE
-	, readDoubleLE
-	, writeFloatLE
-	, writeDoubleLE} from './float/mod.ts'
-
+import {
+	readDoubleLE,
+	readFloatLE,
+	writeDoubleLE,
+	writeFloatLE,
+} from './float/mod.ts'
 
 const SHIFT_LEFT_32 = (1 << 16) * (1 << 16)
 const SHIFT_RIGHT_32 = 1 / SHIFT_LEFT_32
@@ -10,7 +11,6 @@ const SHIFT_RIGHT_32 = 1 / SHIFT_LEFT_32
 // Threshold chosen based on both benchmarking and knowledge about browser string
 // data structures (which currently switch structure types at 12 bytes or more)
 const TEXT_DECODER_MIN_LENGTH = 12
-const utf8TextDecoder = typeof TextDecoder === 'undefined' ? null : new TextDecoder('utf-8')
 
 const unsigned = false
 
@@ -26,21 +26,24 @@ class ProtoBuf {
 	type	: number
 	length	: number
 
-	constructor (buf?: Uint8Array) {
+	constructor(buf?: Uint8Array) {
 		this.buf	= buf || new Uint8Array
 		this.pos	= 0
 		this.type	= 0
 		this.length	= this.buf.length
 	}
 
-	destroy = () => {
-		this.buf = new Uint8Array
+	destroy() {
+		this.buf = new Uint8Array()
 	}
 
 	// === READING =================================================================
 
-	readFields = <T>(readField: (tag: number, result: T, pbf: ProtoBuf) => void, result: T, end = this.length) => {
-
+	readFields<T>(
+		readField: (tag: number, result: T, pbf: ProtoBuf) => void,
+		result: T,
+		end = this.length,
+	) {
 		while (this.pos < end) {
 			const val	= this.readVarint(unsigned)
 			const tag	= val >> 3
@@ -54,17 +57,24 @@ class ProtoBuf {
 		return result
 	}
 
-	readMessage = <T>(readField: (tag: number, result: T, pbf: ProtoBuf) => void, result: T) => {
-		return this.readFields(readField, result, this.readVarint(unsigned) + this.pos)
+	readMessage<T>(
+		readField: (tag: number, result: T, pbf: ProtoBuf) => void,
+		result: T,
+	) {
+		return this.readFields(
+			readField,
+			result,
+			this.readVarint(unsigned) + this.pos,
+		)
 	}
 
-	readFixed32 = () => {
+	readFixed32() {
 		const val = readUInt32(this.buf, this.pos)
 		this.pos += 4
 		return val
 	}
 
-	readSFixed32 = () => {
+	readSFixed32() {
 		const val = readInt32(this.buf, this.pos)
 		this.pos += 4
 		return val
@@ -72,31 +82,31 @@ class ProtoBuf {
 
 	// 64-bit int handling is based on github.com/dpw/node-buffer-more-ints (MIT-licensed)
 
-	readFixed64 = () => {
+	readFixed64() {
 		const val = readUInt32(this.buf, this.pos) + readUInt32(this.buf, this.pos + 4) * SHIFT_LEFT_32
 		this.pos += 8
 		return val
 	}
 
-	readSFixed64 = () => {
+	readSFixed64() {
 		const val = readUInt32(this.buf, this.pos) + readInt32(this.buf, this.pos + 4) * SHIFT_LEFT_32
 		this.pos += 8
 		return val
 	}
 
-	readFloat = () => {
+	readFloat() {
 		const val = readFloatLE(this.buf, this.pos)
 		this.pos += 4
 		return val
 	}
 
-	readDouble = () => {
+	readDouble() {
 		const val = readDoubleLE(this.buf, this.pos)
 		this.pos += 8
 		return val
 	}
 
-	readVarint = (isSigned: boolean) => {
+	readVarint(isSigned: boolean) {
 		const buf = this.buf
 		let val
 		let b
@@ -123,49 +133,53 @@ class ProtoBuf {
 		return readVarintRemainder(val, isSigned, this)
 	}
 
-	readVarint64 = () => { // for compatibility with v2.0.1
+	readVarint64() { // for compatibility with v2.0.1
 		return this.readVarint(true)
 	}
 
-	readSVarint = () => {
+	readSVarint() {
 		const num = this.readVarint(unsigned)
 		return num % 2 === 1 ? (num + 1) / -2 : num / 2 // zigzag encoding
 	}
 
-	readBoolean = () => {
+	readBoolean() {
 		return Boolean(this.readVarint(unsigned))
 	}
 
-	readString = () => {
+	readString() {
 		const end = this.readVarint(unsigned) + this.pos
 		const pos = this.pos
 		this.pos = end
 
 		// longer strings are fast with the built-in browser TextDecoder API
-		if (end - pos >= TEXT_DECODER_MIN_LENGTH && utf8TextDecoder) return readUtf8TextDecoder(this.buf, pos, end)
+		if (end - pos >= TEXT_DECODER_MIN_LENGTH) {
+			return readUtf8TextDecoder(this.buf, pos, end)
+		}
 
 		// short strings are fast with our custom implementation
 		return readUtf8(this.buf, pos, end)
 	}
 
-	readBytes = () => {
+	readBytes() {
 		const end = this.readVarint(unsigned) + this.pos
 		const buffer = this.buf.subarray(this.pos, end)
 		this.pos = end
 		return buffer
 	}
 
-	// verbose for performance reasons; doesn't affect gzipped size
-	
-	readPackedVarint = (arr: Array<number>, isSigned: boolean) => {
-		if (this.type !== ProtoBuf.Bytes) return arr.push(this.readVarint(isSigned))
+	// verbose for performance reasons doesn't affect gzipped size
+
+	readPackedVarint(arr: Array<number>, isSigned: boolean) {
+		if (this.type !== ProtoBuf.Bytes) {
+			return arr.push(this.readVarint(isSigned))
+		}
 		const end = readPackedEnd(this)
 		arr = arr || []
 		while (this.pos < end) arr.push(this.readVarint(isSigned))
 		return arr
 	}
-	
-	readPackedSVarint = (arr: Array<number>) => {
+
+	readPackedSVarint(arr: Array<number>) {
 		if (this.type !== ProtoBuf.Bytes) return arr.push(this.readSVarint())
 		const end = readPackedEnd(this)
 		arr = arr || []
@@ -173,7 +187,7 @@ class ProtoBuf {
 		return arr
 	}
 
-	readPackedBoolean = (arr: Array<boolean>) => {
+	readPackedBoolean(arr: Array<boolean>) {
 		if (this.type !== ProtoBuf.Bytes) return arr.push(this.readBoolean())
 		const end = readPackedEnd(this)
 		arr = arr || []
@@ -181,15 +195,15 @@ class ProtoBuf {
 		return arr
 	}
 
-	readPackedFloat = (arr: Array<number>) => {
+	readPackedFloat(arr: Array<number>) {
 		if (this.type !== ProtoBuf.Bytes) return arr.push(this.readFloat())
 		const end = readPackedEnd(this)
 		arr = arr || []
 		while (this.pos < end) arr.push(this.readFloat())
 		return arr
 	}
-	
-	readPackedDouble = (arr: Array<number>) => {
+
+	readPackedDouble(arr: Array<number>) {
 		if (this.type !== ProtoBuf.Bytes) return arr.push(this.readDouble())
 		const end = readPackedEnd(this)
 		arr = arr || []
@@ -197,7 +211,7 @@ class ProtoBuf {
 		return arr
 	}
 
-	readPackedFixed32 = (arr: Array<number>) => {
+	readPackedFixed32(arr: Array<number>) {
 		if (this.type !== ProtoBuf.Bytes) return arr.push(this.readFixed32())
 		const end = readPackedEnd(this)
 		arr = arr || []
@@ -205,7 +219,7 @@ class ProtoBuf {
 		return arr
 	}
 
-	readPackedSFixed32 = (arr: Array<number>) => {
+	readPackedSFixed32(arr: Array<number>) {
 		if (this.type !== ProtoBuf.Bytes) return arr.push(this.readSFixed32())
 		const end = readPackedEnd(this)
 		arr = arr || []
@@ -213,7 +227,7 @@ class ProtoBuf {
 		return arr
 	}
 
-	readPackedFixed64 = (arr: Array<number>) => {
+	readPackedFixed64(arr: Array<number>) {
 		if (this.type !== ProtoBuf.Bytes) return arr.push(this.readFixed64())
 		const end = readPackedEnd(this)
 		arr = arr || []
@@ -221,7 +235,7 @@ class ProtoBuf {
 		return arr
 	}
 
-	readPackedSFixed64 = (arr: Array<number>) => {
+	readPackedSFixed64(arr: Array<number>) {
 		if (this.type !== ProtoBuf.Bytes) return arr.push(this.readSFixed64())
 		const end = readPackedEnd(this)
 		arr = arr || []
@@ -229,7 +243,7 @@ class ProtoBuf {
 		return arr
 	}
 
-	skip = (val: number) => {
+	skip(val: number) {
 		const type = val & 0x7
 		// deno-lint-ignore no-empty
 		if	(type === ProtoBuf.Varint)	while (this.buf[this.pos++] > 0x7f) {}
@@ -241,11 +255,11 @@ class ProtoBuf {
 
 	// === WRITING =================================================================
 
-	writeTag = (tag: number, type: number) => {
+	writeTag(tag: number, type: number) {
 		this.writeVarint((tag << 3) | type)
 	}
 
-	realloc = (min: number) => {
+	realloc(min: number) {
 		let length = this.length || 16
 
 		while (length < this.pos + min) length *= 2
@@ -258,39 +272,39 @@ class ProtoBuf {
 		}
 	}
 
-	finish = () => {
+	finish() {
 		this.length = this.pos
 		this.pos = 0
 		return this.buf.subarray(0, this.length)
 	}
 
-	writeFixed32 = (val: number) => {
+	writeFixed32(val: number) {
 		this.realloc(4)
 		writeInt32(this.buf, val, this.pos)
 		this.pos += 4
 	}
 
-	writeSFixed32 = (val: number) => {
+	writeSFixed32(val: number) {
 		this.realloc(4)
 		writeInt32(this.buf, val, this.pos)
 		this.pos += 4
 	}
 
-	writeFixed64 = (val: number) => {
+	writeFixed64(val: number) {
 		this.realloc(8)
 		writeInt32(this.buf, val & -1, this.pos)
 		writeInt32(this.buf, Math.floor(val * SHIFT_RIGHT_32), this.pos + 4)
 		this.pos += 8
 	}
 
-	writeSFixed64 = (val: number) => {
+	writeSFixed64(val: number) {
 		this.realloc(8)
 		writeInt32(this.buf, val & -1, this.pos)
 		writeInt32(this.buf, Math.floor(val * SHIFT_RIGHT_32), this.pos + 4)
 		this.pos += 8
 	}
 
-	writeVarint = (val: number | boolean) => {
+	writeVarint(val: number | boolean) {
 		val = Number(val)
 
 		if (val > 0xfffffff || val < 0) {
@@ -309,18 +323,18 @@ class ProtoBuf {
 		this.buf[this.pos++] = ((val >>>= 7) & 0x7f) | (val > 0x7f ? 0x80 : 0)
 		if (val <= 0x7f) return
 		
-		this.buf[this.pos++] =  (val >>>  7) & 0x7f
+		this.buf[this.pos++] = ((val >>>  7) & 0x7f)
 	}
 
-	writeSVarint = (val: number) => {
+	writeSVarint(val: number) {
 		this.writeVarint(val < 0 ? -val * 2 - 1 : val * 2)
 	}
 
-	writeBoolean = (val: boolean) => {
+	writeBoolean(val: boolean) {
 		this.writeVarint(val)
 	}
 
-	writeString = (str: string) => {
+	writeString(str: string) {
 		this.realloc(str.length * 4)
 
 		this.pos++ // reserve 1 byte for short string length
@@ -338,26 +352,26 @@ class ProtoBuf {
 		this.pos += len
 	}
 
-	writeFloat = (val: number) => {
+	writeFloat(val: number) {
 		this.realloc(4)
 		writeFloatLE(val, this.buf, this.pos)
 		this.pos += 4
 	}
 
-	writeDouble = (val: number) => {
+	writeDouble(val: number) {
 		this.realloc(8)
 		writeDoubleLE(val, this.buf, this.pos)
 		this.pos += 8
 	}
 
-	writeBytes = (buffer: Uint8Array) => {
+	writeBytes(buffer: Uint8Array) {
 		const len = buffer.length
 		this.writeVarint(len)
 		this.realloc(len)
 		for (let i = 0; i < len; i++) this.buf[this.pos++] = buffer[i]
 	}
 
-	writeRawMessage = <T> (fn: (obj: T, pbf: ProtoBuf) => void, obj: T) => {
+	writeRawMessage<T>(fn: (obj: T, pbf: ProtoBuf) => void, obj: T) {
 		this.pos++ // reserve 1 byte for short message length
 
 		// write the message directly to the buffer and see how much was written
@@ -373,72 +387,76 @@ class ProtoBuf {
 		this.pos += len
 	}
 
-	writeMessage = <T> (tag: number, fn: (obj: T, pbf: ProtoBuf) => void, obj: T) => {
+	writeMessage<T>(
+		tag: number,
+		fn: (obj: T, pbf: ProtoBuf) => void,
+		obj: T,
+	) {
 		this.writeTag(tag, ProtoBuf.Bytes)
 		this.writeRawMessage(fn, obj)
 	}
 
-	writePackedBoolean	= (tag: number, arr: Array<boolean>)=> { if (arr.length) this.writeMessage(tag, writePackedBoolean	, arr) }
-	writePackedVarint	= (tag: number, arr: Array<number>) => { if (arr.length) this.writeMessage(tag, writePackedVarint	, arr) }
-	writePackedSVarint	= (tag: number, arr: Array<number>) => { if (arr.length) this.writeMessage(tag, writePackedSVarint	, arr) }
-	writePackedFloat	= (tag: number, arr: Array<number>) => { if (arr.length) this.writeMessage(tag, writePackedFloat	, arr) }
-	writePackedDouble	= (tag: number, arr: Array<number>) => { if (arr.length) this.writeMessage(tag, writePackedDouble	, arr) }
-	writePackedFixed32	= (tag: number, arr: Array<number>) => { if (arr.length) this.writeMessage(tag, writePackedFixed32	, arr) }
-	writePackedSFixed32	= (tag: number, arr: Array<number>) => { if (arr.length) this.writeMessage(tag, writePackedSFixed32	, arr) }
-	writePackedFixed64	= (tag: number, arr: Array<number>) => { if (arr.length) this.writeMessage(tag, writePackedFixed64	, arr) }
-	writePackedSFixed64	= (tag: number, arr: Array<number>) => { if (arr.length) this.writeMessage(tag, writePackedSFixed64	, arr) }
+	writePackedBoolean	(tag: number, arr: Array<boolean>){ if (arr.length) this.writeMessage(tag, writePackedBoolean	, arr) }
+	writePackedVarint	(tag: number, arr: Array<number>) { if (arr.length) this.writeMessage(tag, writePackedVarint	, arr) }
+	writePackedSVarint	(tag: number, arr: Array<number>) { if (arr.length) this.writeMessage(tag, writePackedSVarint	, arr) }
+	writePackedFloat	(tag: number, arr: Array<number>) { if (arr.length) this.writeMessage(tag, writePackedFloat	, arr) }
+	writePackedDouble	(tag: number, arr: Array<number>) { if (arr.length) this.writeMessage(tag, writePackedDouble	, arr) }
+	writePackedFixed32	(tag: number, arr: Array<number>) { if (arr.length) this.writeMessage(tag, writePackedFixed32	, arr) }
+	writePackedSFixed32	(tag: number, arr: Array<number>) { if (arr.length) this.writeMessage(tag, writePackedSFixed32	, arr) }
+	writePackedFixed64	(tag: number, arr: Array<number>) { if (arr.length) this.writeMessage(tag, writePackedFixed64	, arr) }
+	writePackedSFixed64	(tag: number, arr: Array<number>) { if (arr.length) this.writeMessage(tag, writePackedSFixed64	, arr) }
 
-	writeBytesField = (tag: number, buffer: Uint8Array) => {
+	writeBytesField(tag: number, buffer: Uint8Array) {
 		this.writeTag(tag, ProtoBuf.Bytes)
 		this.writeBytes(buffer)
 	}
 
-	writeFixed32Field = (tag: number, val: number) => {
+	writeFixed32Field(tag: number, val: number) {
 		this.writeTag(tag, ProtoBuf.Fixed32)
 		this.writeFixed32(val)
 	}
 
-	writeSFixed32Field = (tag: number, val: number) => {
+	writeSFixed32Field(tag: number, val: number) {
 		this.writeTag(tag, ProtoBuf.Fixed32)
 		this.writeSFixed32(val)
 	}
 
-	writeFixed64Field = (tag: number, val: number) => {
+	writeFixed64Field(tag: number, val: number) {
 		this.writeTag(tag, ProtoBuf.Fixed64)
 		this.writeFixed64(val)
 	}
 
-	writeSFixed64Field = (tag: number, val: number) => {
+	writeSFixed64Field(tag: number, val: number) {
 		this.writeTag(tag, ProtoBuf.Fixed64)
 		this.writeSFixed64(val)
 	}
 
-	writeVarintField = (tag: number, val: number | boolean) => {
+	writeVarintField(tag: number, val: number | boolean) {
 		this.writeTag(tag, ProtoBuf.Varint)
 		this.writeVarint(val)
 	}
 
-	writeSVarintField = (tag: number, val: number) => {
+	writeSVarintField(tag: number, val: number) {
 		this.writeTag(tag, ProtoBuf.Varint)
 		this.writeSVarint(val)
 	}
 
-	writeStringField = (tag: number, str: string) => {
+	writeStringField(tag: number, str: string) {
 		this.writeTag(tag, ProtoBuf.Bytes)
 		this.writeString(str)
 	}
 
-	writeFloatField = (tag: number, val: number) => {
+	writeFloatField(tag: number, val: number) {
 		this.writeTag(tag, ProtoBuf.Fixed32)
 		this.writeFloat(val)
 	}
 
-	writeDoubleField = (tag: number, val: number) => {
+	writeDoubleField(tag: number, val: number) {
 		this.writeTag(tag, ProtoBuf.Fixed64)
 		this.writeDouble(val)
 	}
 
-	writeBooleanField = (tag: number, val: boolean) => {
+	writeBooleanField(tag: number, val: boolean) {
 		this.writeVarintField(tag, Boolean(val))
 	}
 }
@@ -476,8 +494,9 @@ function readVarintRemainder(low: number, isSigned: boolean, pbf: ProtoBuf) {
 }
 
 function readPackedEnd(pbf: ProtoBuf) {
-	return pbf.type === ProtoBuf.Bytes ?
-	pbf.readVarint(unsigned) + pbf.pos : pbf.pos + 1
+	return pbf.type === ProtoBuf.Bytes
+		? pbf.readVarint(unsigned) + pbf.pos
+		: pbf.pos + 1
 }
 
 function toNum(low: number, high: number, isSigned: boolean) {
@@ -497,14 +516,18 @@ function writeBigVarint(val: number, pbf: ProtoBuf) {
 		low  = ~(-val % 0x100000000)
 		high = ~(-val / 0x100000000)
 
-		if (low ^ 0xffffffff)  low = (low + 1) | 0
+		if (low ^ 0xffffffff) {
+			low = (low + 1) | 0
+		}
 		else {
-			low = 0
+			low  = 0
 			high = (high + 1) | 0
 		}
 	}
 
-	if (val >= 0x10000000000000000 || val < -0x10000000000000000) throw new Error('Given varint doesn\'t fit into 10 bytes')
+	if (val >= 0x10000000000000000 || val < -0x10000000000000000) {
+		throw new Error('Given varint doesn\'t fit into 10 bytes')
+	}
 
 	pbf.realloc(10)
 
@@ -561,8 +584,11 @@ function makeRoomForExtraLength(startPos: number, len: number, pbf: ProtoBuf) {
 		(len <= 0xfffffff) ? 3 : Math.floor(Math.log(len) / (Math.LN2 * 7))
 
 	// if 1 byte isn't enough for encoding message length, shift the data to the right
-	pbf.realloc(extraLen);
-	for (let i = pbf.pos - 1; i >= startPos; i--) pbf.buf[i + extraLen] = pbf.buf[i]
+	pbf.realloc(extraLen)
+
+	for (let i = pbf.pos - 1; i >= startPos; i--) {
+		pbf.buf[i + extraLen] = pbf.buf[i]
+	}
 }
 
 function writePackedBoolean	(arr: Array<boolean>,pbf: ProtoBuf) { for (let i = 0; i < arr.length; i++) pbf.writeBoolean	(arr[i]) }
@@ -579,23 +605,23 @@ function writePackedSFixed64	(arr: Array<number>, pbf: ProtoBuf) { for (let i = 
 
 function readUInt32(buf: Uint8Array, pos: number) {
 	return ((buf[pos]) |
-	(buf[pos + 1] << 8) |
-	(buf[pos + 2] << 16)) +
-	(buf[pos + 3] * 0x1000000)
+		(buf[pos + 1] << 8) |
+		(buf[pos + 2] << 16)) +
+		(buf[pos + 3] * 0x1000000)
 }
 
 function writeInt32(buf: Uint8Array, val: number, pos: number) {
 	buf[pos] = val
-	buf[pos + 1] = (val >>> 8)
-	buf[pos + 2] = (val >>> 16)
-	buf[pos + 3] = (val >>> 24)
+	buf[pos + 1] = val >>> 8
+	buf[pos + 2] = val >>> 16
+	buf[pos + 3] = val >>> 24
 }
 
 function readInt32(buf: Uint8Array, pos: number) {
 	return ((buf[pos]) |
-	(buf[pos + 1] << 8) |
-	(buf[pos + 2] << 16)) +
-	(buf[pos + 3] << 24)
+		(buf[pos + 1] << 8) |
+		(buf[pos + 2] << 16)) +
+		(buf[pos + 3] << 24)
 }
 
 function readUtf8(buf: Uint8Array, pos: number, end: number) {
@@ -610,9 +636,9 @@ function readUtf8(buf: Uint8Array, pos: number, end: number) {
 		let c: number | null = null // codepoint
 
 		let bytesPerSequence =
-			b0 > 0xEF ? 4 :
-			b0 > 0xDF ? 3 :
-			b0 > 0xBF ? 2 : 1
+			(b0 > 0xEF) ? 4 :
+			(b0 > 0xDF) ? 3 :
+			(b0 > 0xBF) ? 2 : 1
 
 		if (i + bytesPerSequence > end) break
 
@@ -636,7 +662,11 @@ function readUtf8(buf: Uint8Array, pos: number, end: number) {
 			b1 = buf[i + 1]
 			b2 = buf[i + 2]
 			b3 = buf[i + 3]
-			if ((b1 & 0xC0) === 0x80 && (b2 & 0xC0) === 0x80 && (b3 & 0xC0) === 0x80) {
+			if (
+				(b1 & 0xC0) === 0x80 &&
+				(b2 & 0xC0) === 0x80 &&
+				(b3 & 0xC0) === 0x80
+			) {
 				c = (b0 & 0xF) << 0x12 | (b1 & 0x3F) << 0xC | (b2 & 0x3F) << 0x6 | (b3 & 0x3F)
 				if (c <= 0xFFFF || c >= 0x110000) c = null
 			}
@@ -656,7 +686,7 @@ function readUtf8(buf: Uint8Array, pos: number, end: number) {
 		i += bytesPerSequence
 	}
 
-    return str
+	return str
 }
 
 function readUtf8TextDecoder(buf: Uint8Array, pos: number, end: number) {
@@ -666,7 +696,7 @@ function readUtf8TextDecoder(buf: Uint8Array, pos: number, end: number) {
 function writeUtf8(buf: Uint8Array, str: string, pos: number) {
 	for (let i = 0, c, lead; i < str.length; i++) {
 		c = str.charCodeAt(i) // code point
-    
+		
 		if (c > 0xD7FF && c < 0xE000) {
 			if (lead) {
 				if (c < 0xDC00) {
@@ -686,7 +716,7 @@ function writeUtf8(buf: Uint8Array, str: string, pos: number) {
 					buf[pos++] = 0xBD
 				}
 				else lead = c
-			
+				
 				continue
 			}
 		} else if (lead) {
@@ -696,8 +726,8 @@ function writeUtf8(buf: Uint8Array, str: string, pos: number) {
 			lead = null
 		}
 		if (c < 0x80) buf[pos++] = c
-	    	else {
-			if (c < 0x800) buf[pos++] = c >> 0x6 | 0xC0;
+		else {
+			if (c < 0x800) buf[pos++] = c >> 0x6 | 0xC0
 			else {
 				if (c < 0x10000) buf[pos++] = c >> 0xC | 0xE0
 				else {
